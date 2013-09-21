c814d94b4b93d142f396374f609ca5979aac7edc
mil <miles.sandlar@gmail.com>
2013-09-21 08:24:42 -0400

    some changes

--- js/bookmarklet.js
+++ js/bookmarklet.js
@@ -1,8 +1,3 @@
-load_js("http://github.com/mil/drunk-browse/master/js/underscore.min.js");
-load_js("http://github.com/mil/drunk-browse/master/js/raphael-min.js");
-load_js("http://github.com/mil/drunk-browse/master/js/bubble_cursor.js");
-load_css("http://github.com/mil/drunk-browse/master/css/style.css");
-
 function load_js(url) {
   var script = document.createElement("script");
   script.setAttribute("type", "text/javascript");
@@ -15,5 +10,8 @@ function load_css(url) {
   stylesheet.setAttribute("type", "text/css");
   stylesheet.setAttribute("href", url);
 }
-
 console.log("ran");
+load_js("http://github.com/mil/drunk-browse/master/js/underscore.min.js");
+load_js("http://github.com/mil/drunk-browse/master/js/raphael-min.js");
+load_js("http://github.com/mil/drunk-browse/master/js/bubble_cursor.js");
+load_css("http://github.com/mil/drunk-browse/master/css/style.css");
